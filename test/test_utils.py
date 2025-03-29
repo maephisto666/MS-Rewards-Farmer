2d459ac8406a9365863a61bffff8677837df51d7
Kyrela <mathis.vinay@gmail.com>
2025-03-29 01:35:00 +0100

    refactor: remove `sendNotification`
    - remove `sendNotification` in favor of `Apprise.notify` directly
    - remove apprise notify assertion
    - remove stacktrace from apprise notifications
    - exit 1 on single account error

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from src.utils import CONFIG, sendNotification
+from src.utils import CONFIG, APPRISE
 
 
 class TestUtils(TestCase):
     def test_send_notification(self):
         CONFIG.apprise.enabled = True
-        sendNotification("title", "body")
+        APPRISE.notify("body", "title")
